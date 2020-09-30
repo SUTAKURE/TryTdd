5963eaa760b4ae993479c2bc15c7ccd9f9461476
SUTAKURE <udonnamerou@gmail.com>
2020-09-30 18:53:29 +0900

    MoneyTest.javaの作成

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,8 @@
-public class App {
+package money;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
