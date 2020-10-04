591ccdb931896be4c4b4936967ca986c34848c14
SUTAKURE <udonnamerou@gmail.com>
2020-10-04 18:46:46 +0900

    check equality

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -8,5 +8,8 @@ int amount;
     public Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
     }
-    
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
 }
